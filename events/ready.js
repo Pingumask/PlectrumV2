89a8fb2d003861f1fc4df9c9dacdf586c745558d
Pingumask <pingumask@elanta.fr>
2021-09-23 19:03:37 +0200

    Personalisation du "Ready"

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = async (client) => {
-    console.log(`Connecté en tant que ${client.user.username}. Prêt sur ${client.guilds.cache.size} serveurs`);
-    client.user.setActivity('Fait des tests');
+    console.log(`Connecté en tant que ${client.user.tag} sur ${client.guilds.cache.size} serveurs`);
+    client.user.setActivity("le Links Squad", { type: "LISTENING"});
 };
