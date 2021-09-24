c51a9d3189a0599e40b5965bdf2b3bd8834690a9
Pingumask <pingumask@elanta.fr>
2021-09-24 14:59:25 +0200

    typo dans ready

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
     const now = new Date();
-    console.log(`${now.toLocaleDateString()} ${now.toLocaleTimeString()}) Connecté en tant que ${client.user.tag} sur ${client.guilds.cache.size} serveurs`);
+    console.log(`(${now.toLocaleDateString()} ${now.toLocaleTimeString()}) Connecté en tant que ${client.user.tag} sur ${client.guilds.cache.size} serveurs`);
     client.user.setActivity("le Links Squad", { type: "LISTENING"});
 };
