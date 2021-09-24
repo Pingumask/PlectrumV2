68e7993c06dc4b37ccdb0ef4dc7570c02eef1bb6
Pingumask <pingumask@elanta.fr>
2021-09-24 15:41:46 +0200

    Ajout de réponses à la divination

--- commands/fun/divination.js
+++ commands/fun/divination.js
@@ -16,15 +16,20 @@ module.exports = {
             "Oui",
             "Bien sûr",
             "Tout à fait",
+            "Tutafeh",
             
             "Ca se pourrait",
             "Peut être",
             "C'est pas impossible",
             "Ptêt bein qu'oui, Ptêt bein qu'non",
+            "Mais j'en sais rien moi !",
+            "Je t'en pose des questions ?",
+            "C'est pas toi qui décide !",
 
             "Lol nope",
             "Non",
             "Et la marmotte, elle met le chocolat dans le papier alu",
+            "Ha ha ha... Non !"
         ];
         const reponse = phrases[Math.floor(Math.random()*phrases.length)];
         const question = interaction.options.getString('question');
