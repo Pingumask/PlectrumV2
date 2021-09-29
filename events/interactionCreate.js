c7aebbd8afa928dac661d189b32a519fac112e44
Pingumask <pingumask@elanta.fr>
2021-09-29 19:45:42 +0200

    Protection générale contre les parametres longs

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -6,7 +6,9 @@ module.exports = async (client, interaction) => {
         args += option.value;
     });
     const cmd = client.commands.get(command) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command));
-    console.log(`(${now.toLocaleDateString()} ${now.toLocaleTimeString()}) ${interaction.guild.name}, #${interaction.channel.name}, @${interaction.member.displayName} : /${command} ${args}` );     
+    console.log(`(${now.toLocaleDateString()} ${now.toLocaleTimeString()}) ${interaction?.guild?.name}, #${interaction?.channel?.name ?? interaction?.channel?.thread?.name}, @${interaction?.member?.displayName} : /${command} ${args}` );   
+    let toolong = interaction.options._hoistedOptions.find(opt=>opt.value.length>1500);       
+    if (toolong) return await interaction.reply({ content: `La valeur envoyée pour ${toolong.name} est trop longue pour être interprétée`, ephemeral: true })
     if (cmd) return cmd.execute(client, interaction);
     return interaction.reply("NOPE !");
 };
